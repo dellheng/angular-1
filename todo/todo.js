453e59360ddfe21d9e8cf6380852d19e7c1542a9
张衡 <zhangheng@lianchuang.com>
2015-02-12 16:05:26 +0800

    jslint

--- todo/todo.js
+++ todo/todo.js
@@ -1,30 +1,33 @@
-var app =  angular.module('todoApp',[]);
+var app = angular.module('todoApp', []);
 
-app.controller('TodoController',function($scope){
-        $scope.todos = [
-            {text:'learn angular', done:true},
-            {text:'build an angular app', done:false}
-        ];
+app.controller('TodoController', function ($scope) {
+    "use strict";
+    $scope.todos = [
+        {text: 'learn angular', done: true},
+        {text: 'build an angular app', done: false}
+    ];
 
-        $scope.addTodo = function(){
-            $scope.todos.push({text:$scope.todoText, done:false});
-            $scope.todoText = '';
-        };
+    $scope.addTodo = function () {
+        $scope.todos.push({text: $scope.todoText, done: false});
+        $scope.todoText = '';
+    };
 
-        $scope.remaining =  function() {
-            var count = 0;
-            angular.forEach($scope.todos, function(todo){
-                count += todo.done ?0 :1;
-            });
+    $scope.remaining =  function () {
+        var count = 0;
+        angular.forEach($scope.todos, function (todo) {
+            count += todo.done ? 0 : 1;
+        });
 
-            return count;
-        }
+        return count;
+    };
 
-        $scope.archive = function() {
-            var oldTodos = $scope.todos;
-            $scope.todos = [];
-            angular.forEach(oldTodos, function(todo){
-               if(!todo.done) $scope.todos.push(todo)
-            });
-        }
-})
+    $scope.archive = function () {
+        var oldTodos = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(oldTodos, function (todo) {
+            if (!todo.done) {
+                $scope.todos.push(todo);
+            }
+        });
+    };
+});
